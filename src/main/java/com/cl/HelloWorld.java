3bc9bb6f942d2fd4b3d476eb91d5acab2996a343
cl <1475518380@qq.com>
2018-07-01 16:43:30 +0800

    src

---
+++ src/main/java/com/cl/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.cl;
+
+/**
+ * Created by Administrator on 2018/7/1 0001.
+ */
+public class HelloWorld {
+    public void main(String[] args) {
+        System.out.print("hello world!");
+    }
+}
