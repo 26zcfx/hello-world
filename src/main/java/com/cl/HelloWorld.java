ea25d3fc05f91a82ace2c1041306a097b67eb4d9
陈磊 <cl@abc.com>
2018-07-02 14:25:51 +0800

    第二次修改

--- src/main/java/com/cl/HelloWorld.java
+++ src/main/java/com/cl/HelloWorld.java
@@ -5,6 +5,7 @@ package com.cl;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("hello world22!");
+		String str = "hello world";
+        System.out.print(str);
     }
 }
